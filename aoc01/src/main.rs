c65783ef0aea5d726362b7e39dcf0c784db2b200
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-21 16:01:58 -0300

    Add my solution to aoc01

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+use std::path;
+
 fn main() {
-    println!("Hello, world!");
+    let input_path: path::PathBuf = "input/input.txt".into();
+    let input = fs::read_to_string(input_path).unwrap();
+
+    let mut frequency = 0;
+    for line in input.lines() {
+        frequency += line.parse::<i32>().unwrap();
+    }
+
+    println!("Frequency is: {}", frequency);
 }
