9e0bb7a3d23ff147099694580c0f46ae8efa2b0d
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-22 15:22:01 -0300

    Improve code quality for aoc02

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,7 +1,27 @@
-use std::{io::{self, Write, Read, BufRead}, collections::HashMap};
-use std::iter::zip;
+use std::{io::{self, Write, Read}, collections::HashMap};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    part1(&input)?;
+    part2(&input)?;
+    Ok(())
+}
+
+fn part1(input: &String) -> Result<()> {
+    let mut doubles = 0;
+    let mut triples = 0;
+    for line in input.lines() {
+        let (doubles_change, triples_change) = count_doubles_and_triples(&line.to_string());
+        doubles += doubles_change;
+        triples += triples_change;
+    }
+    writeln!(io::stdout(), "Checkum is: {}", doubles * triples)?;
+    Ok(())
+}
 
 fn count_doubles_and_triples(input: &String) -> (u32, u32) {
     let mut has_doubles: u32 = 0;
@@ -21,46 +41,42 @@ fn count_doubles_and_triples(input: &String) -> (u32, u32) {
     (has_doubles, has_triples)
 }
 
-fn part1(input: &String) -> Result<()> {
-    let mut doubles = 0;
-    let mut triples = 0;
-    for line in input.lines() {
-        let (doubles_change, triples_change) = count_doubles_and_triples(&line.to_string());
-        doubles += doubles_change;
-        triples += triples_change;
-    }
-    writeln!(io::stdout(), "Checkum is: {}", doubles * triples)?;
-    Ok(())
-}
 
-fn part2(input: &String) -> Result<()> {
-    let mut words: Vec<String> = Vec::new();
-    for word in input.lines() {
-        words.push(word.to_string());
-    }
-    for (i, word) in words.iter().enumerate() {
-        for other_word in &words[i..] {
-            let mut diff_count = 0;
-            for (c1, c2) in zip(word.chars(), other_word.chars()) {
-                if c1 != c2 {
-                    diff_count += 1;
-                }
-            }
-            if diff_count == 1 {
-                writeln!(io::stdout(), "Word: {} Other word: {}", word, other_word)?;
+fn part2(input: &str) -> Result<()> {
+    let ids: Vec<&str> = input.lines().collect();
+
+    for i in 0..ids.len() {
+        for j in i+1..ids.len() {
+            if let Some(common_letters) = common_letters_one_diff(ids[i], ids[j]) {
+                writeln!(io::stdout(), "Common letters: {}", common_letters)?;
                 return Ok(());
             }
         }
     }
 
-    Ok(())
+    Err(From::from("could not find two correct box ids"))
 }
 
-fn main() -> Result<()> {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
+fn common_letters_one_diff(id1: &str, id2: &str) -> Option<String> {
+    if id1.len() != id2.len() {
+        return None;
+    }
 
-    part1(&input)?;
-    part2(&input)?;
-    Ok(())
+    let mut diff_counter = 0;
+    for (c1, c2) in id1.chars().zip(id2.chars()) {
+        if c1 != c2 {
+            diff_counter += 1;
+            if diff_counter > 1 {
+                return None;
+            }
+        }
+    }
+
+    Some(
+        id1.chars().zip(id2.chars())
+            .filter(|&(c1, c2)| c1 == c2)
+            .map(|(c, _)| c)
+            .collect()
+    )
 }
+
