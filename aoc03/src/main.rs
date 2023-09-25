7a33338d79945f15ecd5078b2a8e08e37c2918ac
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-25 18:27:32 -0300

    Add solution for part 1

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -11,6 +11,8 @@ use std::{
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
+type Grid = HashMap<(u32, u32), u32>;
+
 macro_rules! err {
     ($($tt:tt)*) => { Err(Box::<dyn Error>::from(format!($($tt)*))) }
 }
@@ -22,9 +24,13 @@ fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
+    let mut grid = Grid::new();
     let claims = parse_lines_into_claims(&input)?;
+    for claim in &claims {
+        populate_grid(claim, &mut grid);
+    }
 
-    part1(&input)?;
+    part1(&grid)?;
     // part2(&input)?;
     Ok(())
 }
@@ -40,12 +46,17 @@ fn parse_lines_into_claims(input: &str) -> Result<Vec<Claim>> {
     Ok(claims)
 }
 
-fn part1(input: &str) -> Result<()> {
-    // Get size of board
-    // let num_rows, num_cols = get_num_rows_and_cols()
-    // Mark board up
-    // Count overlaps on board
+fn populate_grid(claim: &Claim, grid: &mut Grid) {
+    for x in claim.x..claim.x + claim.width {
+        for y in claim.y..claim.y + claim.height {
+            *grid.entry((x, y)).or_default() += 1;
+        }
+    }
+}
 
+fn part1(grid: &Grid) -> Result<()> {
+    let count = grid.values().filter(|&&count| count >= 2).count();
+    writeln!(io::stdout(), "Number of claims with count of 2 or more: {}", count)?;
     Ok(())
 }
 
