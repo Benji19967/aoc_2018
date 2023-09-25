abec8685821f5d33bc7e89306d869467a25b2fe4
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-25 16:42:06 -0300

    Format code

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -1,8 +1,13 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::{io::{self, Write, Read}, collections::HashMap, str::FromStr, error::Error};
 use regex::Regex;
+use std::{
+    collections::HashMap,
+    error::Error,
+    io::{self, Read, Write},
+    str::FromStr,
+};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
@@ -17,25 +22,29 @@ fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
+    let claims = parse_lines_into_claims(&input)?;
+
     part1(&input)?;
     // part2(&input)?;
     Ok(())
 }
 
+fn parse_lines_into_claims(input: &str) -> Result<Vec<Claim>> {
+    let mut claims = Vec::new();
+    for line in input.lines() {
+        let claim: Claim = line
+            .parse()
+            .or_else(|err| err!("failed to parse '{:?}': {}", line, err))?;
+        claims.push(claim);
+    }
+    Ok(claims)
+}
 
 fn part1(input: &str) -> Result<()> {
     // Get size of board
     // let num_rows, num_cols = get_num_rows_and_cols()
     // Mark board up
     // Count overlaps on board
-
-    for line in input.lines() {
-        println!("Line: {}", line);
-        let claim: Claim = line.parse().or_else(|err| {
-            err!("failed to parse '{:?}': {}", line, err)
-        })?;
-        println!("Claim: {:?}", claim);
-    }
 
     Ok(())
 }
@@ -46,15 +55,6 @@ fn get_num_rows_and_cols(input: &str) -> (u32, u32) {
     //     id, dist_left, dist_top, width, height = parse_line(&line);
     // }
     (0, 0)
-}
-
-fn parse_line(line: &str) -> (u32, u32, u32, u32, u32) {
-    // #123 @ 3,2: 5x4
-    let (mut id, mut dist_left, dist_top, width, height) = (0, 0, 0, 0, 0);
-    for word in line.split_whitespace() {
-        let x = 5;
-    }
-    (0, 0, 0, 0, 0)
 }
 
 #[derive(Debug)]
@@ -68,27 +68,29 @@ struct Claim {
 
 impl FromStr for Claim {
     type Err = Box<dyn Error>;
-    
+
     fn from_str(s: &str) -> Result<Claim> {
-        // lazy_static makes sure the compiler can optimize and only compiles the 
+        // lazy_static makes sure the compiler can optimize and only compiles the
         // regex once, even if this function is used in a loop.
         lazy_static! {
-            static ref RE: Regex = Regex::new(r"(?x)
+            static ref RE: Regex = Regex::new(
+                r"(?x)
                 \#
                 (?P<id>[0-9]+)
                 \s+@\s+
                 (?P<x>[0-9]+),(?P<y>[0-9]+):
                 \s+
-                (?P<width>[0-9]+)x(?P<height>[0-9]+)
-            ").unwrap();
+                (?P<width>[0-9]+)x(?P<height>[0-9]+)"
+            )
+            .unwrap();
         }
         let caps = RE.captures(s).ok_or("Regex failed to capture groups")?;
-        Ok(Claim { 
-            id: caps["id"].parse()?, 
-            x: caps["x"].parse()?, 
-            y: caps["y"].parse()?, 
-            width: caps["width"].parse()?, 
-            height: caps["height"].parse()?, 
+        Ok(Claim {
+            id: caps["id"].parse()?,
+            x: caps["x"].parse()?,
+            y: caps["y"].parse()?,
+            width: caps["width"].parse()?,
+            height: caps["height"].parse()?,
         })
     }
 }
@@ -103,5 +105,3 @@ impl FromStr for Claim {
 //...........
 //...........
 //...........
-
-
