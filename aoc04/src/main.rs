2cd82648e740af775f782171e404a50ad50a8cce
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-26 17:29:48 -0300

    Add reading the file

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write, Read};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    for line in input.lines() {
+        writeln!(io::stdout(), "{}", line)?;
+    }
+
+    // part1(&input)?;
+    // part2(&input)?;
+
+    Ok(())
 }
